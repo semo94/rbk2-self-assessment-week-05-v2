ebd40e400cc7fe4a9e32adcf5304628450aa93ea
semo94 <saleem.bakri94@gmail.com>
2017-02-04 09:58:00 +0200

    not working

--- node-express-sequelize/server.js
+++ node-express-sequelize/server.js
@@ -4,15 +4,21 @@ var Sequelize = require('sequelize');
 var sequelize = new Sequelize('test', 'test', null, { dialect: 'sqlite', storage: './db.test.sqlite' });
 
 var User = sequelize.define('User', {
-  username: Sequelize.STRING
+	username: Sequelize.STRING
 });
 
 /*  Create a '/users' route that responds to 
-    a GET request with all users in the database */
+a GET request with all users in the database */
 
-
+app.get('/users', function(req, res){
+	models.User.findAll({
+		include: [ User.username ]
+	}).then(function(users) {
+		res.render(JSON.stringify(users));
+	});
+});
 
 module.exports = { 
-  app: app,
-  User: User
+	app: app,
+	User: User
 };
